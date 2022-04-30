feee61d8ae65d7638e54078f07e87db9a82db1e5
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 08:56:07 -0500

    Agregadas las carpetas de app y test, archivo para la clase e index, insertado el sript start para usar npm start

---
+++ index.js
@@ -0,0 +1,12 @@
+const MissionCommander = require("./app/MissionCommander");
+
+
+
+objeto1 = new MissionCommander("Juan");
+objeto2 = new MissionCommander("Pedro");
+objeto3 = new MissionCommander("Pablo");
+
+
+console.log(objeto1.name);
+console.log(objeto2.name);
+console.log(objeto3.name);
